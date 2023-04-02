2ee808e2942e9ae8502fc8c75a894deb3c53b389
picarenlamina <fernandodomin.ruiz@murciaeduca.es>
2023-04-02 21:31:04 +0200

    202304022131

--- scripts/init.sql
+++ scripts/init.sql
@@ -3,6 +3,8 @@ CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'password';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'exporter'@'localhost';
 */
+/*
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'user'@'mysql';
 GRANT SELECT ON performance_schema.* TO 'user''mysql';
 COMMIT;
+*/
