4234fbd6465cd6c9a6a7bea3c0efd04043d225d3
picarenlamina <fernandodomin.ruiz@murciaeduca.es>
2023-04-02 21:06:47 +0200

    202304022040

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,7 +1,8 @@
--- Creacion de usuario para exportacion
---CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'password';
---GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'localhost';
---GRANT SELECT ON performance_schema.* TO 'exporter'@'localhost';
-GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'user'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'user'@'localhost';
+/* Creacion de usuario para exportacion
+CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'password';
+GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'exporter'@'localhost';
+*/
+GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'user'@'mysql';
+GRANT SELECT ON performance_schema.* TO 'user''mysql';
 COMMIT;
